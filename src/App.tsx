4fb0a644f33779e376a85009b5f4e3dc3b639297
frndhoon <frndhoon@gmail.com>
2024-11-12 13:43:43 +0900

    feat: 금융 상품 백엔드 api 요청 및 응답 처리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,56 @@ import { useState, useEffect } from 'react';
 import ky from 'ky';
 import Header from './components/Header';
 import Card from './components/Card';
+import { FinanceProduct } from './models/finance-product.interface';
 
 function App() {
-  const API_KEY = '3aa82da50c82362230f42d308463d03c';
-  const [finProducts, setFinProducts] = useState({});
-  const [error, setError] = useState(null);
+  const API_URL = 'http://localhost:8080/finance/deposit';
+  const [finProducts, setFinProducts] = useState<FinanceProduct[] | null>(null);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await ky
-          .get(
-            'https://cors-anywhere.herokuapp.com/http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json',
-            {
-              searchParams: {
-                auth: API_KEY,
-                topFinGrpNo: '020000',
-                pageNo: '1',
-              },
-            }
-          )
-          .json();
+          .get(API_URL, {
+            searchParams: {
+              topFinGrpNo: '020000',
+              pageNo: '1',
+            },
+          })
+          .json<FinanceProduct[]>();
 
-        setFinProducts(response as {});
+        setFinProducts(response);
       } catch (err) {
         console.error('Failed to fetch data:', err);
-        setError(err as any);
+        setError(err instanceof Error ? err : new Error('Unknown error'));
       }
     };
     fetchData();
   }, []);
 
+  const renderProductList = () => {
+    if (error) {
+      return <p>Failed to load data: {error.message}</p>;
+    }
+
+    if (!finProducts) {
+      return <p>Loading...</p>;
+    }
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        {finProducts.map((product) => (
+          <Card key={product.finPrdtCd} product={product} />
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <main className="flex flex-col items-center">
+      <main className="flex flex-col items-center gap-10">
         <Header />
-        <Card />
+        {renderProductList()}
       </main>
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,25 @@
-export default function Card() {
+import { FinanceProduct } from '../models/finance-product.interface';
+
+export default function Card({ product }: { product: FinanceProduct }) {
+  const { korCoNm, finPrdtNm, joinWay, mtrtInt } = product;
   return (
-    <div className="p-10 shadow-md">
-      <div>
-        <div>product</div>
-        <div>dcls_month</div>
-        <div>fin_co_no</div>
-        <div>fin_prdt_cd</div>
+    <div className="max-w-sm mx-auto p-6 min-w-40 bg-white rounded-lg shadow-lg border border-gray-200">
+      <div className="space-y-4">
+        {/* korCoNum */}
+        <div className="text-gray-500 text-sm">금융회사 명</div>
+        <div className="font-semibold text-xl text-gray-800">{korCoNm}</div>
+
+        {/* finPrdtNm */}
+        <div className="text-gray-500 text-sm">금융상품 명</div>
+        <div className="font-semibold text-xl text-gray-800">{finPrdtNm}</div>
+
+        {/* joinWay */}
+        <div className="text-gray-500 text-sm">가입 방법</div>
+        <div className="text-lg text-gray-800">{joinWay}</div>
+
+        {/* mtrInt */}
+        <div className="text-gray-500 text-sm">만기 후 이자율</div>
+        <div className="text-lg text-gray-800">{mtrtInt}</div>
       </div>
     </div>
   );
