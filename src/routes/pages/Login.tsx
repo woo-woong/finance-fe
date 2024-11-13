dc3bf133b48fad87db5c53b05f8bd6136e7b0f1d
frndhoon <frndhoon@gmail.com>
2024-11-13 11:26:45 +0900

    style: 로그인 페이지 ui 수정

--- src/routes/pages/Login.tsx
+++ src/routes/pages/Login.tsx
@@ -3,7 +3,7 @@ import kakaoLogo from '../../assets/images/kakao-login.png';
 export default function Login() {
   return (
     <div className="flex flex-col items-center p-20 rounded-md shadow-md">
-      <h1 className="mb-10 text-xl font-bold">로그인</h1>
+      <h1 className="text-xl font-bold mb-7">로그인</h1>
       <form action="" className="flex flex-col items-center">
         <div className="flex flex-col gap-2 mb-5">
           <input
@@ -17,12 +17,18 @@ export default function Login() {
             className="p-2 border border-gray-40"
           />
         </div>
-        <button type="button" className="p-3 bg-blue-200 border rounded-sm">
+        <button
+          type="button"
+          className="w-full p-3 transition-colors bg-blue-200 border rounded-sm cursor-pointer hover:bg-blue-400"
+        >
           로그인
         </button>
-        <h1 className="text-lg font-bold">SNS 로그인</h1>
-        <img src={kakaoLogo} alt="kakao-login" className="w-10" />
-        <div>카카오톡</div>
+        <hr className="w-full my-5 border-t border-gray-300" />
+        <div className="flex flex-col items-center gap-2 mt-3">
+          <h1 className="text-lg font-bold">SNS 로그인</h1>
+          <img src={kakaoLogo} alt="kakao-login" className="w-12" />
+          <p className="text-sm">카카오톡</p>
+        </div>
       </form>
     </div>
   );
