ee75e23773bc70ea90fe6ab1335daa1fb6dbe37e
frndhoon <frndhoon@gmail.com>
2024-11-13 22:45:02 +0900

    feat: 회원가입 페이지 로직 컴포넌트로 분리

--- src/routes/pages/SignUp.tsx
+++ src/routes/pages/SignUp.tsx
@@ -1,158 +1,125 @@
 import { useEffect, useState } from 'react';
 import DaumAddress from '../../components/DaumAddress';
+import SignUpFormInput from '../../components/SignUpFormInput';
 
 export default function SignUp() {
-  const [address, setAddress] = useState('');
-  const [detailAddress, setDetailAddress] = useState('');
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    name: '',
+    birthDate: '',
+    email: '',
+    phone: '',
+    address: '',
+    detailAddress: '',
+  });
 
-  const handleChangeDetailAddress = (
-    e: React.ChangeEvent<HTMLInputElement>
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleAddressChange: React.Dispatch<React.SetStateAction<string>> = (
+    value
   ) => {
-    setDetailAddress(e.target.value);
+    if (typeof value === 'string') {
+      setFormData((prevData) => ({
+        ...prevData,
+        address: value,
+      }));
+    }
   };
 
   useEffect(() => {
-    setDetailAddress('');
-  }, [address]);
+    setFormData((prevFormData) => ({ ...prevFormData, detailAddress: '' }));
+  }, [formData.address]);
 
   return (
     <div className="flex justify-center w-full">
       <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-lg">
         <h2 className="mb-6 text-2xl font-bold text-center">회원가입</h2>
         <form>
-          <div className="mb-4">
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium text-gray-700"
-            >
-              아이디
-              <input
-                type="text"
-                id="username"
-                name="username"
-                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </label>
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              비밀번호
-              <input
-                type="password"
-                id="password"
-                name="password"
-                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </label>
-          </div>
+          <SignUpFormInput
+            label="아이디"
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+          />
 
-          <div className="mb-4">
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium text-gray-700"
-            >
-              이름
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </label>
-          </div>
+          <SignUpFormInput
+            label="비밀번호"
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
 
-          <div className="mb-4">
-            <label
-              htmlFor="dob"
-              className="block text-sm font-medium text-gray-700"
-            >
-              생년월일
-              <input
-                type="date"
-                id="birthDate"
-                name="birthDate"
-                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </label>
-          </div>
+          <SignUpFormInput
+            label="이름"
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
 
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              이메일
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </label>
-          </div>
+          <SignUpFormInput
+            label="생년월일"
+            type="date"
+            id="birthDate"
+            name="birthDate"
+            value={formData.birthDate}
+            onChange={handleInputChange}
+          />
 
-          <div className="mb-4">
-            <label
-              htmlFor="phoneNumber"
-              className="block text-sm font-medium text-gray-700"
-            >
-              전화번호
-              <input
-                type="tel"
-                id="phone"
-                name="phone"
-                className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </label>
-          </div>
+          <SignUpFormInput
+            label="이메일"
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
 
-          <div className="mb-4">
-            <label
-              htmlFor="address"
-              className="block text-sm font-medium text-gray-700"
-            >
-              주소
-              <input
-                type="text"
-                id="address"
-                name="address"
-                className="block w-full px-3 py-2 mt-1 mb-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-                value={address}
-                disabled
-              />
-            </label>
-            <label
-              htmlFor="address"
-              className="block text-sm font-medium text-gray-700"
-            >
-              상세 주소
-              <input
-                type="text"
-                id="detailAddress"
-                name="detailAddress"
-                className="block w-full px-3 py-2 mt-1 mb-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-                value={detailAddress}
-                onChange={handleChangeDetailAddress}
-                disabled={address.length === 0}
-              />
-            </label>
+          <SignUpFormInput
+            label="전화번호"
+            type="tel"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleInputChange}
+          />
+          <div className="flex flex-col space-y-0">
+            <SignUpFormInput
+              label="주소"
+              type="text"
+              id="address"
+              name="address"
+              value={formData.address}
+              onChange={handleInputChange}
+              disabled
+            />
 
-            <DaumAddress setAddress={setAddress} />
+            <SignUpFormInput
+              label="상세 주소"
+              type="text"
+              id="detailAddress"
+              name="detailAddress"
+              value={formData.detailAddress}
+              onChange={handleInputChange}
+              disabled={formData.address.length === 0}
+            />
+            <DaumAddress setAddress={handleAddressChange} />
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
+            className="w-full px-4 py-2 mt-3 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
           >
             회원가입
           </button>
