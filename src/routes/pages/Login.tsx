f5691a8ea1ee8823b12b5cc35dac41fad933b8bc
frndhoon <frndhoon@gmail.com>
2024-11-13 10:57:31 +0900

    feat: 기본 로그인 페이지 ui 추가

--- src/routes/pages/Login.tsx
+++ src/routes/pages/Login.tsx
@@ -1,3 +1,27 @@
 export default function Login() {
-  return <p>login</p>;
+  return (
+    <div className="flex flex-col items-center p-20 rounded-md shadow-md">
+      <h1 className="mb-10 text-xl font-bold">로그인</h1>
+      <form action="" className="flex flex-col items-center">
+        <div className="flex flex-col gap-2 mb-5">
+          <input
+            type="text"
+            placeholder="아이디"
+            className="p-2 border border-gray-40"
+          />
+          <input
+            type="text"
+            placeholder="비밀번호"
+            className="p-2 border border-gray-40"
+          />
+        </div>
+        <button type="button" className="p-3 bg-blue-200 border rounded-sm">
+          로그인
+        </button>
+        <h1 className="text-lg font-bold">SNS 로그인</h1>
+        <img src="" alt="" />
+        <div>카카오톡</div>
+      </form>
+    </div>
+  );
 }
