2f9a85e6fb8b3a74a7e345b4b79c4e537944cd6b
JeongHoon Woo <frndhoon@gmail.com>
2024-11-13 09:33:46 +0900

    ♻ 금융 상품 데이터 api custom hook으로 동일 로직 분리 (#1)
    * refactor: 금융 API 요청 custom hook 생성 및 로직 분리
    
    * refactor: useFinProducts 커스텀 훅 사용
    JSON.stringify로 객체 내부 값의 변경 여부 확인하여 불필요한 useEffect
    실행  제어
    useMemo를 이용해 동일한 문자열을 참조하도록 설정

---
+++ src/hooks/useFinProducts.ts
@@ -0,0 +1,57 @@
+import { useState, useEffect, useMemo } from 'react';
+import ky from 'ky';
+import { FinanceProduct } from '../models/finance-product.interface';
+
+export default function useFinProducts({
+  path,
+  searchParams = {},
+  defaultVisibleCount = 3,
+}: {
+  path: string;
+  searchParams: Record<string, string>;
+  defaultVisibleCount: number;
+}) {
+  const API_URL = `http://localhost:8080/finance/${path}`;
+  const memoizedSearchParams = useMemo(
+    () => searchParams,
+    [JSON.stringify(searchParams)]
+  );
+
+  const [finProducts, setFinProducts] = useState<FinanceProduct[] | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [visibleCount, setVisibleCount] = useState(defaultVisibleCount);
+
+  const loadMoreProducts = () => {
+    setVisibleCount((prevCount) => prevCount + defaultVisibleCount);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await ky
+          .get(API_URL, {
+            searchParams: memoizedSearchParams,
+          })
+          .json<FinanceProduct[]>();
+
+        setFinProducts(response);
+      } catch (err) {
+        console.error('Failed to fetch data:', err);
+        setError(err instanceof Error ? err : new Error('Unknown error'));
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [path, memoizedSearchParams]);
+
+  return {
+    finProducts,
+    error,
+    visibleCount,
+    isLoading,
+    loadMoreProducts,
+  };
+}
--- src/routes/pages/Deposit.tsx
+++ src/routes/pages/Deposit.tsx
@@ -1,48 +1,27 @@
-import { useState, useEffect } from 'react';
-import ky from 'ky';
 import Card from '../../components/Card';
-import { FinanceProduct } from '../../models/finance-product.interface';
+import useFinProducts from '../../hooks/useFinProducts';
 
-function Deposit() {
-  const API_URL = 'http://localhost:8080/finance/deposit';
-  const DEFAULT_VISIBLE_COUNT = 3;
-  const [finProducts, setFinProducts] = useState<FinanceProduct[] | null>(null);
-  const [error, setError] = useState<Error | null>(null);
-  const [visibleCount, setVisibleCount] = useState(DEFAULT_VISIBLE_COUNT);
-
-  const loadMoreProducts = () => {
-    setVisibleCount((prevCount) => prevCount + DEFAULT_VISIBLE_COUNT);
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await ky
-          .get(API_URL, {
-            searchParams: {
-              topFinGrpNo: '020000',
-              pageNo: '1',
-            },
-          })
-          .json<FinanceProduct[]>();
-
-        setFinProducts(response);
-      } catch (err) {
-        console.error('Failed to fetch data:', err);
-        setError(err instanceof Error ? err : new Error('Unknown error'));
-      }
-    };
-    fetchData();
-  }, []);
+export default function Deposit() {
+  const { finProducts, error, visibleCount, isLoading, loadMoreProducts } =
+    useFinProducts({
+      path: 'deposit',
+      searchParams: { topFinGrpNo: '020000', pageNo: '1' },
+      defaultVisibleCount: 3,
+    });
 
   const renderProductList = () => {
     if (error) {
       return <p>Failed to load data: {error.message}</p>;
     }
 
-    if (!finProducts) {
+    if (isLoading) {
       return <p>Loading...</p>;
     }
+
+    if (!finProducts) {
+      return <p>No products available.</p>;
+    }
+
     return (
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
         {finProducts.slice(0, visibleCount).map((product) => (
@@ -67,5 +46,3 @@ function Deposit() {
     </>
   );
 }
-
-export default Deposit;
