2e6e121cc1087a1a17d4a10e51a7a1ea76f2c62d
frndhoon <frndhoon@gmail.com>
2024-11-14 09:24:23 +0900

    feat: 로그인 api post 기능 추가(더미 데이터)

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,35 @@
+import ky from 'ky';
+export default function useLogin() {
+  const API_URL = 'https://example.com/api';
+
+  const login = async (username: any, password: any) => {
+    try {
+      const response = await ky
+        .post(`${API_URL}/login`, {
+          json: {
+            username,
+            password,
+          },
+        })
+        .json();
+
+      const { token }: any = response;
+      localStorage.setItem('token', token), console.log('로그인 성공', token);
+    } catch (error) {
+      console.error('로그인 실패', error);
+    }
+  };
+
+  const handleLoginClick = () => {
+    const username = 'test';
+    const password = 'test';
+    login(username, password);
+  };
+
+  const apiWithAuth = ky.create({
+    prefixUrl: API_URL,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
+}
