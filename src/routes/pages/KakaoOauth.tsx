275a8115c2450a0c6baae8baa31d73f43c76c4f5
frndhoon <frndhoon@gmail.com>
2024-11-15 17:55:26 +0900

    feat: jwt cookie 확인

--- src/routes/pages/KakaoOauth.tsx
+++ src/routes/pages/KakaoOauth.tsx
@@ -1,10 +1,13 @@
 import ky from 'ky';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useAuthStore from '@stores/authStore';
+import Cookies from 'universal-cookie';
 
 export default function KakaoOauth() {
   const code = new URL(window.location.href).searchParams.get('code');
   const navigate = useNavigate();
+  const cookies = new Cookies();
 
   useEffect(() => {
     const fetchToken = async () => {
@@ -14,7 +17,8 @@ export default function KakaoOauth() {
           credentials: 'include', // 쿠키 포함 요청
         });
 
-        // 메인 페이지로 리디렉션
+        const jwt = cookies.get('jwt');
+
         navigate('/');
       } catch (e) {
         console.error('토큰 요청 실패:', e);
@@ -24,7 +28,7 @@ export default function KakaoOauth() {
     if (code) {
       fetchToken();
     }
-  }, [code, navigate]); // `code`와 `navigate` 의존성 추가
+  }, [code, navigate, cookies]);
 
-  return null; // 이 컴포넌트는 UI를 렌더링하지 않음
+  return null;
 }
